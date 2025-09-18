786e700a0bc4aaec101ed5aee85b5ac46fa88272
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2025-09-19 01:15:56 +0900

    Time: 6 ms (51.41%), Space: 45.6 MB (44.95%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        List<String> result = new ArrayList<>();
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+
+        for(List<String> ticket : tickets){
+            map.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            map.get(ticket.get(0)).add(ticket.get(1));
+        }
+
+        dfs(result, map, "JFK");
+
+        return result;
+    }
+
+    void dfs(List<String> result, Map<String, PriorityQueue<String>> map, String from){
+        
+        while(map.containsKey(from) && !map.get(from).isEmpty()){
+            dfs(result, map, map.get(from).poll());
+        }
+
+        result.add(0, from);
+    }
+}
