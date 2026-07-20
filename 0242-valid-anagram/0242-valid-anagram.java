d9f9f9c0cf481bdf36129c540f93f7fed7db9334
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-20 23:59:53 +0900

    Time: 2 ms (99.6%), Space: 44.7 MB (55.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 class Solution {
+
+    int[] sAlphas = new int[26];
+    int[] tAlphas = new int[26];
+
     public boolean isAnagram(String s, String t) {
-        char[] sA = s.toCharArray();
-        char[] tA = t.toCharArray();
-
-        Arrays.sort(sA);
-        Arrays.sort(tA);
-
-        String a = "", b = "";
-        for(char c : sA){
-            a += c;
-        }
-
-        for(char c : tA){
-            b += c;
-        }
-
-        if(a.equals(b)){
-            return true;
-        }
         
-        return false;
+        for (char c : s.toCharArray()) {
+            sAlphas[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            tAlphas[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (sAlphas[i] != tAlphas[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
