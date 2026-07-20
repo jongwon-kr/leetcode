373ffabfc403bba517beac3b91fc5b60647f0b25
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-21 00:08:17 +0900

    Time: 6 ms (84.39%), Space: 46.7 MB (85.18%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,15 +1,15 @@
 class Solution {
     public int firstUniqChar(String s) {
         // 입력 받은 문자열의 문자 개수 카운트
-        int[] cntArr = new int[26];
+        int[] freq = new int[26];
 
         for (char c : s.toCharArray()) {
-            cntArr[c - 'a']++;
+            freq[c - 'a']++;
         }
 
         for (int i = 0; i < s.length(); i++) {
             // 다시 순회하면서 반복되지 않은 문자일 경우 인덱스 반환
-            if (cntArr[s.charAt(i) - 'a'] == 1) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
