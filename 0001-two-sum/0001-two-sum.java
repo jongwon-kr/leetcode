0c66236d0ccfc8c941cafb6eabcda3a24bc624a9
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-21 11:18:34 +0900

    Time: 2 ms (99.29%), Space: 47.4 MB (11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        int[] answer = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
+        // HashMap에<값, 인덱스>를 활용
 
         for (int i = 0; i < nums.length; i++) {
-            int correct = target - nums[i];
-            if (map.containsKey(correct)) {
-                result[0] = i;
-                result[1] = map.get(correct);
+            // 찾는 값
+            int findTarget = target - nums[i];
+            // 합이 target이 되는 값이 존재하면 i와 findTarget의 인덱스 리턴
+            if (map.containsKey(findTarget)) {
+                answer[0] = i;
+                answer[1] = map.get(findTarget);
+                break;
             }
             map.put(nums[i], i);
         }
-        
-        return result;
+        return answer;
     }
 }
