0aad14a00d9d73967e5c883025864599659adbff
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-26 13:39:00 +0900

    Time: 6 ms (20.69%), Space: 51.6 MB (81.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
-    boolean[][] visited;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n = 0, m = 0;
 
     public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        // 섬 개수
+        int cntLand = 0;
 
-        visited = new boolean[grid.length][grid[0].length];
-
-        int islandCount = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(i, j, grid);
-                    islandCount++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 방문하지 않은 육지
+                if (!visited[i][j] && grid[i][j] =='1') {
+                    bfs(grid, j, i);
+                    cntLand++;
                 }
             }
         }
 
-        return islandCount;
+        return cntLand;
     }
 
-    void dfs(int i, int j, char[][] grid) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0' || visited[i][j]) {
-            return;
+    public void bfs(char[][] grid, int x, int y) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        visited[y][x] = true;
+        q.offer(new int[]{x, y});
+        
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= n || nx < 0 || ny >= m || ny < 0) continue;
+                if (grid[ny][nx] == '0' || visited[ny][nx]) continue;
+
+                visited[ny][nx] = true;
+                q.offer(new int[]{nx, ny});
+            }
         }
-
-        visited[i][j] = true;
-
-        dfs(i + 1, j, grid);
-        dfs(i - 1, j, grid);
-        dfs(i, j + 1, grid);
-        dfs(i, j - 1, grid);
     }
 }
