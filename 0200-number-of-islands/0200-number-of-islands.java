58c89efdf0793565ad626b4132195ddfe7ec40bc
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-26 13:43:17 +0900

    Time: 4 ms (46.29%), Space: 51.6 MB (81.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 class Solution {
     
-    static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int n = 0, m = 0;
@@ -11,14 +10,13 @@ class Solution {
     public int numIslands(char[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        visited = new boolean[m][n];
         // 섬 개수
         int cntLand = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 // 방문하지 않은 육지
-                if (!visited[i][j] && grid[i][j] =='1') {
+                if (grid[i][j] =='1') {
                     bfs(grid, j, i);
                     cntLand++;
                 }
@@ -30,7 +28,7 @@ class Solution {
 
     public void bfs(char[][] grid, int x, int y) {
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        visited[y][x] = true;
+        grid[y][x] = '0';
         q.offer(new int[]{x, y});
         
         while (!q.isEmpty()) {
@@ -40,9 +38,9 @@ class Solution {
                 int ny = cur[1] + dy[i];
 
                 if (nx >= n || nx < 0 || ny >= m || ny < 0) continue;
-                if (grid[ny][nx] == '0' || visited[ny][nx]) continue;
+                if (grid[ny][nx] == '0') continue;
 
-                visited[ny][nx] = true;
+                grid[ny][nx] = '0';
                 q.offer(new int[]{nx, ny});
             }
         }
