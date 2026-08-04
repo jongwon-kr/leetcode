715e719ceed5c902b9c66bf983eac94ab8d790f1
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-08-04 17:39:17 +0900

    Time: 3 ms (26.65%), Space: 47.7 MB (75.93%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,64 +2,60 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-
     static int m, n;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public void solve(char[][] board) {
         if (board == null || board.length == 0) return;
-        // 가장자리 먼저 체크 후 임시로 다른 문자로 치환한 다음 남아 있는 O를 전부 캡쳐하고 다시 S를 O로 변경
+
         m = board.length;
         n = board[0].length;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
 
-        int j = 0, k = n - 1;
         for (int i = 0; i < m; i++) {
-            if (board[i][j] == 'O') {
-                dfs(i, j, board);
+            if (board[i][0] == 'O') {
+                board[i][0] = 'S';
+                q.offer(new int[]{i, 0});
             }
-
-            if (board[i][k] == 'O') {
-                dfs(i, k, board);
+            if (board[i][n - 1] == 'O') {
+                board[i][n - 1] = 'S';
+                q.offer(new int[]{i, n - 1});
             }
         }
 
-        int o = 0, p = m - 1;
-        for (int u = 0; u < n; u++) {
-            if (board[o][u] == 'O') {
-                dfs(o, u, board);
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                board[0][j] = 'S';
+                q.offer(new int[]{0, j});
             }
+            if (board[m - 1][j] == 'O') {
+                board[m - 1][j] = 'S';
+                q.offer(new int[]{m - 1, j});
+            }
+        }
 
-            if (board[p][u] == 'O') {
-                dfs(p, u, board);
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || board[nx][ny] != 'O') continue;
+
+                board[nx][ny] = 'S';
+                q.offer(new int[]{nx, ny});
             }
         }
 
         for (int a = 0; a < m; a++) {
             for (int b = 0; b < n; b++) {
-                if (board[a][b] == 'O') {
-                    board[a][b] = 'X';
-                }
+                if (board[a][b] == 'O') board[a][b] = 'X';
+                else if (board[a][b] == 'S') board[a][b] = 'O';
             }
         }
-
-        for (int a = 0; a < m; a++) {
-            for (int b = 0; b < n; b++) {
-                if (board[a][b] == 'S') {
-                    board[a][b] = 'O';
-                }
-            }
-        }
-    }
-
-    private void dfs(int i, int j, char[][] board) {
-
-        if (i >= m || i < 0 || j >= n || j < 0) return;
-        if (board[i][j] == 'X' || board[i][j] == 'S') return;
-
-        board[i][j] = 'S';
-
-        dfs(i + 1, j, board);
-        dfs(i - 1, j, board);
-        dfs(i, j + 1, board);
-        dfs(i, j - 1, board);
     }
 }
