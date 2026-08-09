548f9b7bfc95d3bd0a7fe7d82c6c046d53a0aab0
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-08-09 18:41:59 +0900

    Time: 2 ms (33.33%), Space: 45.7 MB (28.98%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,32 @@
-import java.util.*;
-
 class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
+        // 순서 상관없이 가능한 모든 순열 반환 백트래킹
+        int n = nums.length;
+        boolean[] isUsed = new boolean[n];
+        List<Integer> list = new ArrayList<>();
+
+        bt(0, nums, isUsed, list);
+        
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+    private void bt(int depth, int[] nums, boolean[] isUsed, List<Integer> list) {
+
+        if (depth == nums.length) {
+            result.add(new ArrayList<>(list));
             return;
         }
 
-        for (int num : nums) {
-            if (tempList.contains(num)) {
-                continue;
-            }
-
-            tempList.add(num);
-            backtrack(result, tempList, nums);
-            tempList.remove(tempList.size() - 1);
+        for (int i = 0; i < nums.length; i++) {
+            if (isUsed[i]) continue;
+            isUsed[i] = true;
+            list.add(nums[i]);
+            bt(depth + 1, nums, isUsed, list);
+            isUsed[i] = false;
+            list.remove(list.size() - 1);
         }
     }
 }
