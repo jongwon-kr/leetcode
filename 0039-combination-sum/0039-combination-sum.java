f94934362a1a58ec868f152f139b6d06294af81f
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-08-10 14:52:35 +0900

    Time: 2 ms (96.25%), Space: 45.7 MB (68.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,29 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
-    public List<List<Integer>> combinationSum(int[] c, int t) {
-        List<List<Integer>> result = new ArrayList<>();
 
-        dfs(result, c, t, 0, new ArrayList<>());
+    List<List<Integer>> result;
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        dfs(0, 0, target, candidates, new ArrayList<>());
         return result;
     }
 
-    void dfs(List<List<Integer>> result, int[] c, int t, int index, List<Integer> list){
-
-        if(t < 0){
-            return;
-        }
-
-        if(t == 0){
+    private void dfs(int idx, int sum, int target, int[] cand, List<Integer> list) {
+        if (sum == target) {
             result.add(new ArrayList<>(list));
             return;
         }
 
-        for(int i = index; i < c.length; i++){
-            int n = c[i];
-            list.add(n);
-            dfs(result, c, t - n, i, list);
+        if (sum > target) {
+            return;
+        }
+
+        for (int i = idx; i < cand.length; i++) {
+            list.add(cand[i]);
+            dfs(i, sum + cand[i], target, cand, list);
             list.remove(list.size() - 1);
         }
     }
